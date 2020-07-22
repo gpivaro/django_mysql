9f475830997bb860f31e50b3484e2a5e1f297892
Gabriel Pivaro <gabrconc@gmail.com>
2020-07-22 00:41:09 -0500

    Changed to "/"

--- django_mysql/urls.py
+++ django_mysql/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("/", include("temperaturemonitor.urls")),
+    path("", include("temperaturemonitor.urls")),
 ]
